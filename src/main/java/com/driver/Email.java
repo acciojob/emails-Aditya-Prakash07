db780ffb6e0c3cf903189a89b64b6e01a3ff2bc0
Aditya <prakashaditya775@gmail.com>
2023-12-25 13:08:50 +0530

    Completed

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,7 +5,7 @@ public class Email {
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,12 +18,31 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+    public void changePassword(String oldPassword, String newPassword) {
+        // Change password only if the oldPassword is equal to current password
+        // and the new password meets all of the following criteria:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        // 5. It contains at least one special character.
+        //    Any character apart from alphabets and digits is considered a special character
+
+        String currentPassword = getPassword();
+
+        if (oldPassword.equals(currentPassword)) {
+            if (newPassword.length() >= 8 &&
+                    newPassword.matches(".*[A-Z].*") &&
+                    newPassword.matches(".*[a-z].*") &&
+                    newPassword.matches(".*[0-9].*") &&
+                    newPassword.matches(".*[!@#$%^&*()].*")) {
+                this.password = newPassword;
+                System.out.println("Password changed successfully.");
+            } else {
+                System.out.println("Password change failed. Please check the new password criteria.");
+            }
+        } else {
+            System.out.println("Password change failed. Old password does not match.");
+        }
     }
 }
